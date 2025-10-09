d0e7b4977a9445996b7b2bc35beded2fe6a671fb
anton.nazarenko <anton.nazarenko@revolut.com>
2025-10-09 14:11:55 +0400

    RPAY-8240 fix android demos

--- packages/revolut-merchant-card-form-demo/android/app/src/main/java/com/revolutmerchantcardformdemo/MainActivity.kt
+++ packages/revolut-merchant-card-form-demo/android/app/src/main/java/com/revolutmerchantcardformdemo/MainActivity.kt
@@ -4,19 +4,27 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.revolut.merchantcardform.api.CardPaymentLauncherHolder
+import com.revolut.merchantcardform.api.CardPaymentLauncherWrapper
 
-class MainActivity : ReactActivity() {
+class MainActivity : ReactActivity(), CardPaymentLauncherHolder {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  override fun getMainComponentName(): String = "RevolutMerchantCardFormDemo"
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    override fun getMainComponentName(): String = "RevolutMerchantCardFormDemo"
 
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    /**
+     * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+     * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+     */
+    override fun createReactActivityDelegate(): ReactActivityDelegate =
+        DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+    /**
+     * Should be defined unconditionally for future Revolut card payments
+     */
+    override val launcher: CardPaymentLauncherWrapper = CardPaymentLauncherWrapper(this)
+
 }
--- packages/revolut-pay-lite-demo/android/app/src/main/java/com/revolutpaylitedemo/MainActivity.kt
+++ packages/revolut-pay-lite-demo/android/app/src/main/java/com/revolutpaylitedemo/MainActivity.kt
@@ -4,19 +4,27 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.revolut.revolutpaylite.api.RevolutPaymentControllerHolder
+import com.revolut.revolutpaylite.api.RevolutPaymentControllerWrapper
 
-class MainActivity : ReactActivity() {
+class MainActivity : ReactActivity(), RevolutPaymentControllerHolder {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  override fun getMainComponentName(): String = "RevolutPayLiteDemo"
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    override fun getMainComponentName(): String = "RevolutPayLiteDemo"
 
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    /**
+     * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+     * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+     */
+    override fun createReactActivityDelegate(): ReactActivityDelegate =
+        DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+    /**
+     * Should be defined unconditionally for future Revolut Pay payments
+     */
+    override val paymentController: RevolutPaymentControllerWrapper =
+        RevolutPaymentControllerWrapper(this)
 }
